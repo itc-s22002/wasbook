64f0d6df8a1295855479390f08b1a94a2ffe2309
s22002 <s22002>
2024-07-30 15:50:05 +0900

    47終了

---
+++ 47-001.php
@@ -0,0 +1,19 @@
+<?php
+  $url = $url = filter_input(INPUT_GET,'url');
+  if (empty ($url)) {
+	       $url = 'http://example.jp/work/47-003.php';
+	         }
+?>
+<html>
+<head><title>ログインしてください</title></head>
+<body>
+<form action="47-002.php" method="POST">
+ユーザ名<input type="text" name="id"><BR>
+パスワード<input type="password" name="pwd"><BR>
+<input type="hidden" name="url" 
+value="<?php echo htmlspecialchars($url, ENT_COMPAT, 'UTF-8') ?>">
+<input type="submit" value="ログイン">
+</form>
+</body>
+</html>
+
